916d62969c2cc7b1f687d763030c274330e9cb13
Jase Pellerin <jasepellerin@gmail.com>
2021-03-25 22:12:28 -0700

    Configure tools

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,17 @@
-import svelte from 'rollup-plugin-svelte';
 import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
-import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
-import sveltePreprocess from 'svelte-preprocess';
-import typescript from '@rollup/plugin-typescript';
+import { config } from 'dotenv';
 import css from 'rollup-plugin-css-only';
+import livereload from 'rollup-plugin-livereload';
+import replace from '@rollup/plugin-replace';
+import resolve from '@rollup/plugin-node-resolve';
+import svelte from 'rollup-plugin-svelte';
+import sveltePreprocess from 'svelte-preprocess';
+import { terser } from 'rollup-plugin-terser';
+import typescript from '@rollup/plugin-typescript';
 
 const production = !process.env.ROLLUP_WATCH;
+
+console.log(config);
 
 function serve() {
   let server;
@@ -79,7 +83,13 @@ export default {
 
     // If we're building for production (npm run build
     // instead of npm run dev), minify
-    production && terser()
+    production && terser(),
+    replace({
+      preventAssignment: true,
+      __app: JSON.stringify({
+        env: { ...config().parsed }
+      })
+    })
   ],
   watch: {
     clearScreen: false
